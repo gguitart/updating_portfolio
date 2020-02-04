f82073ee1fd71f27d2e666dfd68a23a08e20dfbb
GG Guitart <gertaguitart2019@u.northwestern.edu>
2020-02-04 14:37:49 -0600

    updating shedd project

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ var ProgressBar = require('progressbar.js');
 
 
 // $("about-me").click(function() 
-function aboutme() { 
+function aboutme() {
     // alert("about me selected!");
     $('html,body').animate({
-        scrollTop: $("#about-me-area").offset().top},
+        scrollTop: $("#about-me-area").offset().top
+    },
         'slow');
 };
 
-function selectedWorks() { 
+function selectedWorks() {
     // alert("You chose selected works!");
 
     $('html,body').animate({
-        scrollTop: $("#selectedWorks").offset().top},
+        scrollTop: $("#selectedWorks").offset().top
+    },
         'slow');
 };
 
@@ -31,9 +33,29 @@ var bar = new ProgressBar.Circle(container, {
     trailColor: '#eee',
     trailWidth: 1,
     svgStyle: null
-  });
+});
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
   
-  bar.animate(1.0);
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-opacity-off";
+  }
+
+bar.animate(1.0);
 
 
 
